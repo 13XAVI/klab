a3a21fac83d6e5ded778f1d225e599f4c56f9dbb
13XAVI <101405347+13XAVI@users.noreply.github.com>
2023-01-05 16:59:50 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+function setCookie(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	let expires = "expires="+d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+	let name = cname + "=";
+	let ca = document.cookie.split(';');
+	for(let i = 0; i < ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+  
+  function checkCookie() {
+	let user = getCookie("username");
+	if (user != "") {
+	  alert("Welcome again " + user);
+	} else {
+	  user = prompt("Please enter your name:", "");
+	  if (user != "" && user != null) {
+		setCookie("username", user, 365);
+	  }
+	}
+  }
+
+
+/*function myfunc(event){
+	event.preventDefault();
+
+	var first_name = document.getElementById("first_name").value;
+	var last_name = document.getElementById("last_name").value;
+	var email_id = document.getElementById("email_id").value;
+	var password = document.getElementById("password").value;
+	var confirmPassword = document.getElementById("confirmPassword").value;
+	localStorage.setItem('is_full_name',first_name);
+	localStorage.setItem('is_last_name',last_name);
+	localStorage.setItem('is_Email_Addres',email_id);
+	localStorage.setItem('is_password',password);
+	localStorage.setItem('is_confPassword',confirmPassword);
+}*/
+
+const first_name = document.querySelector("full_name"),
+h3 = document.querySelector("h3");
+
+h3.innerHTML = localStorage.getItem("value");
+
+first_name.addEventListener("fullname",full_name);
+
+function display(){
+	localStorage.setItem('value', input.value);
+	h3.innerHTML = localStorage.getItem("value")
+}
+
+/*Sliders
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+end Sliders*/
+/*popins*/
